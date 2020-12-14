d208e751ef7ccb8a50002e2f217988767493071c
ppenguin <hieronymusv@gmail.com>
2020-12-14 20:04:18 +0100

    specific settings for HP Spectre x360 aw-xx600

--- 2in1screen.c
+++ 2in1screen.c
@@ -6,7 +6,7 @@
 #include <string.h>
 
 #define DATA_SIZE 256
-#define N_STATE 2
+#define N_STATE 4
 char basedir[DATA_SIZE];
 char *basedir_end = NULL;
 char content[DATA_SIZE];
@@ -56,9 +56,11 @@ FILE* bdopen(char const *fname, char leave_open){
 }
 
 void rotate_screen(){
+	printf("rotating screen: %s\n", ROT[current_state]);
 	sprintf(command, "xrandr -o %s", ROT[current_state]);
 	system(command);
-	sprintf(command, "xinput set-prop \"%s\" \"Coordinate Transformation Matrix\" %s", "Wacom HID 4846 Finger", COOR[current_state]);
+	sprintf(command, "xinput set-prop \"%s\" \"Coordinate Transformation Matrix\" %s", "pointer:ELAN2514:00 04F3:29F5", COOR[current_state]);
+	sprintf(command, "xinput set-prop \"%s\" \"Coordinate Transformation Matrix\" %s", "pointer:ELAN2514:00 04F3:29F5 Pen (0)", COOR[current_state]);
 	system(command);
 }
 
